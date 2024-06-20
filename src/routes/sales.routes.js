075642308fc933664a795e15c1eb07b4e6f996eb
Igor <igor.vguerra@hotmail.com>
2024-06-20 14:29:51 +0100

    authorization class project

---
+++ src/routes/sales.routes.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+const SalesController = require("../controllers/SalesController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const verifYUserAuthorization = require("../middlewares/verifyUserAuthorization");
+
+const salesRoutes = Router();
+
+const salesController = new SalesController();
+
+salesRoutes.use(ensureAuthenticated);
+salesRoutes.use(verifYUserAuthorization("admin"));
+
+salesRoutes.get("/", salesController.index);
+
+module.exports = salesRoutes;
